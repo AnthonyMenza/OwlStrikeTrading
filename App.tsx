c41f7f406dc549c9a4b5d3da6392338925b55177
AnthonyMenza <Anthony.menza92@gmail.com>
2025-08-11 22:51:50 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,3 @@
+// src/App.tsx - React skeleton for OwlStrikeTrading UI
+import React from 'react';
+export default function App(){ return (<div style={{background:'#000', color:'#0f0', minHeight:'100vh'}}><h1>OwlStrikeTrading UI (Green/Black theme)</h1></div>); }
